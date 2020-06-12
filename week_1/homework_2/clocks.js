eb0a1ee67ce472407edc0f8e2b1fb0803174bf85
Andrey Tutin <and.tutin@gmail.com>
2020-06-12 16:42:48 +0300

    finished homework_2

--- week_1/homework_2/clocks.js
+++ week_1/homework_2/clocks.js
@@ -3,20 +3,27 @@ require('dotenv').config()
 const tickInterval = process.env.TICK_INTERVAL;
 const clockTime = process.env.CLOCK_TIME;
 const port = process.env.PORT;
+let activeConnections = 0;
+let timerId;
 
 http.createServer((request, response) => {
-    if(request.url === '/'){
+    if(request.url === '/') {
+        if (activeConnections == 0) {
+            timerId = setInterval(() => {
+                console.log(Date.now())
+            }, tickInterval);
+        }
 
-        let timerId = setInterval(() => {
-            console.log(Date.now())
-        }, tickInterval);
+        activeConnections++;
         
         setTimeout(() => {
-            clearInterval(timerId);
             response.setHeader("Content-Type", "text/html; charset=utf-8;");
             response.write(`<h2>Текущее время ${new Date}</h2>`);
             response.end();
+            activeConnections--;
+            if (activeConnections == 0) {
+                clearInterval(timerId);
+            }
         }, clockTime);
-    }
-       
+    }     
 }).listen(port);
